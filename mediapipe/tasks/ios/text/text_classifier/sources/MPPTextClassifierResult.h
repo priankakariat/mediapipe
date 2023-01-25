05afaf5b00cd97e730d52cb1915074d11ac2629f
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2023-01-25 20:11:29 +0530

    Added MPPembedding and MPPEmbeddingResult

--- mediapipe/tasks/ios/text/text_classifier/sources/MPPTextClassifierResult.h
+++ mediapipe/tasks/ios/text/text_classifier/sources/MPPTextClassifierResult.h
@@ -39,6 +39,10 @@ NS_SWIFT_NAME(TextClassifierResult)
 - (instancetype)initWithClassificationResult:(MPPClassificationResult *)classificationResult
                                  timestampMs:(NSInteger)timestampMs;
 
+- (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)new NS_UNAVAILABLE;                                 
+
 @end
 
 NS_ASSUME_NONNULL_END
