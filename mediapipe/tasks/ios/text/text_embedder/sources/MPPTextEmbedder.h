46aa2c204bde4afbf93188f831cf450d3dcafc78
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2023-02-01 18:39:15 +0530

    Updated text embedder documentation

--- mediapipe/tasks/ios/text/text_embedder/sources/MPPTextEmbedder.h
+++ mediapipe/tasks/ios/text/text_embedder/sources/MPPTextEmbedder.h
@@ -21,77 +21,66 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- * @brief Performs classification on text.
+ * @brief Performs embedding extraction on text.
  *
- * This API expects a TFLite model with (optional) [TFLite Model
- * Metadata](https://www.tensorflow.org/lite/convert/metadata")that contains the mandatory
- * (described below) input tensors, output tensor, and the optional (but recommended) label
- * items as AssociatedFiles with type TENSOR_AXIS_LABELS per output classification tensor.
+ * This API expects a TFLite model with (optional) [TFLite Model Metadata](https://www.tensorflow.org/lite/convert/metadata").
  *
- * Metadata is required for models with int32 input tensors because it contains the input
- * process unit for the model's Tokenizer. No metadata is required for models with string
- * input tensors.
+ * Metadata is required for models with int32 input tensors because it contains the input process
+ * unit for the model's Tokenizer. No metadata is required for models with string input tensors.
  *
  * Input tensors
- *  - Three input tensors `kTfLiteInt32` of shape `[batch_size xbert_max_seq_len]`
+ *  - Three input tensors `kTfLiteInt32` of shape `[batch_size x bert_max_seq_len]`
  *    representing the input ids, mask ids, and segment ids. This input signature requires
  *    a Bert Tokenizer process unit in the model metadata.
- *  - Or one input tensor `kTfLiteInt32` of shape `[batch_size xmax_seq_len]` representing
+ *  - Or one input tensor `kTfLiteInt32` of shape `[batch_size x max_seq_len]` representing
  *    the input ids. This input signature requires a Regex Tokenizer process unit in the
  *    model metadata.
  *  - Or one input tensor (`kTfLiteString`) that is shapeless or has shape `[1]` containing
  *    the input string.
  *
- * At least one output tensor (`kTfLiteFloat32/kBool`) with:
- *  - `N` classes and shape `[1 x N]`
- *  - optional (but recommended) label map(s) as AssociatedFiles with type TENSOR_AXIS_LABELS,
- *    containing one label per line. The first such AssociatedFile (if any) is used to fill the
- *    `categoryName` field of the results. The `displayName` field is filled from the
- *    AssociatedFile (if any) whose locale matches the `displayNamesLocale` field of the
- *    `MPPTextClassifierOptions` used at creation time ("en" by default, i.e. English). If none of
- *    these are available, only the `index` field of the results will be filled.
+ * At least one output tensor (`kTfLiteFloat32`/`kTfLiteUint8`) with shape `[1 x N]` where `N` is the number of dimensions in the produced embeddings.
  */
 NS_SWIFT_NAME(TextEmbedder)
 @interface MPPTextEmbedder : NSObject
 
 /**
- * Creates a new instance of `MPPTextClassifier` from an absolute path to a TensorFlow Lite
- * model file stored locally on the device and the default `MPPTextClassifierOptions`.
+ * Creates a new instance of `MPPTextEmbedder` from an absolute path to a TensorFlow Lite
+ * model file stored locally on the device and the default `MPPTextEmbedderOptions`.
  *
  * @param modelPath An absolute path to a TensorFlow Lite model file stored locally on the device.
  * @param error An optional error parameter populated when there is an error in initializing the
- * text classifier.
+ * text embedder.
  *
- * @return A new instance of `MPPTextClassifier` with the given model path. `nil` if there is an
- * error in initializing the text classifier.
+ * @return A new instance of `MPPTextEmbedder` with the given model path. `nil` if there is an
+ * error in initializing the text embedder.
  */
 - (nullable instancetype)initWithModelPath:(NSString *)modelPath error:(NSError **)error;
 
 /**
- * Creates a new instance of `MPPTextClassifier` from the given `MPPTextClassifierOptions`.
+ * Creates a new instance of `MPPTextEmbedder` from the given `MPPTextEmbedderOptions`.
  *
- * @param options The options of type `MPPTextClassifierOptions` to use for configuring the
- * `MPPTextClassifier`.
+ * @param options The options of type `MPPTextEmbedderOptions` to use for configuring the
+ * `MPPTextEmbedder.
  * @param error An optional error parameter populated when there is an error in initializing the
- * text classifier.
+ * text embedder.
  *
- * @return A new instance of `MPPTextClassifier` with the given options. `nil` if there is an
- * error in initializing the text classifier.
+ * @return A new instance of `MPPTextEmbedder` with the given options. `nil` if there is an
+ * error in initializing the text embedder.
  */
 - (nullable instancetype)initWithOptions:(MPPTextEmbedderOptions *)options
                                    error:(NSError **)error NS_DESIGNATED_INITIALIZER;
 
 /**
- * Performs classification on the input text.
+ * Performs embedding extraction on the input text.
  *
- * @param text The `NSString` on which classification is to be performed.
+ * @param text The `NSString` on which embedding extraction is to be performed.
  * @param error An optional error parameter populated when there is an error in performing
- * classification on the input text.
+ * embedding extraction on the input text.
  *
- * @return  A `MPPTextClassifierResult` object that contains a list of text classifications.
+ * @return  A `MPPTextEmbedderResult` object that contains a list of embeddings.
  */
 - (nullable MPPTextEmbedderResult *)embedText:(NSString *)text
-                                             error:(NSError **)error NS_SWIFT_NAME(classify(text:));
+                                             error:(NSError **)error NS_SWIFT_NAME(embed(text:));
 
 - (instancetype)init NS_UNAVAILABLE;
 
