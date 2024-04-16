9745b441116bade9cddc4a0f5ac8a0b1c2ccbe73
Prianka Liz Kariat <prianka.kariat@codeandtheory.com>
2024-04-16 21:18:33 +0530

    Refactored methods in iOS MPPAudioRecord

--- mediapipe/tasks/ios/audio/core/sources/MPPAudioRecord.h
+++ mediapipe/tasks/ios/audio/core/sources/MPPAudioRecord.h
@@ -19,7 +19,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/** 
+/**
  * A wrapper class to record the device's microphone continuously. Currently
  * this class only supports recording upto 2 channels. If the number of channels
  * is 2, then the mono microphone input is duplicated to provide dual channel
@@ -31,7 +31,7 @@ NS_SWIFT_NAME(AudioRecord)
 /** Audio format specifying the number of channels and sample rate supported. */
 @property(nonatomic, readonly) MPPAudioDataFormat *audioDataFormat;
 
-/** 
+/**
  * Size of the buffer held by `AudioRecord`. It ensures delivery of audio
  * data of length `bufferLength` arrays when you start recording the microphone
  * input.
@@ -41,38 +41,29 @@ NS_SWIFT_NAME(AudioRecord)
 /**
  * Initializes a new `AudioRecord` with the given audio format and buffer length.
  *
- * @param format An audio format of type `AudioFormat`.
- * @param bufferLength Maximum number of elements the internal buffer of
- * `AudioRecord` can hold at any given point of time. The buffer length
- * must be a multiple of `format.channelCount`.
- * @param error An optional error parameter populated if the initialization of `AudioRecord` was
- * not successful.
+ * @param format An audio format of type `AudioDataFormat`.
+ * @param bufferLength Maximum number of elements the internal buffer of `AudioRecord` can hold at
+ * any given point of time. The buffer length must be a multiple of `format.channelCount`.
  *
- * @return An new instance of `AudioRecord` with the given audio format and buffer size. `nil` if
+ * @return An new instance of `AudioRecord` with the given audio format and buffer length. `nil` if
  * there is an error in initializing `AudioRecord`.
  */
-- (nullable instancetype)initWithAudioFormat:(MPPAudioDataFormat *)format
-                                bufferLength:(NSUInteger)bufferSize
-                                       error:(NSError **)error;
+- (nullable instancetype)initWithAudioDataFormat:(MPPAudioDataFormat *)format
+                                    bufferLength:(NSUInteger)bufferSize
+                                           error:(NSError **)error;
 
 /**
  * This function starts recording the audio from the microphone if audio record permissions
  * have been granted by the user.
  *
- * @discussion Before calling this function, you must call
- * `-[AVAudioSession requestRecordPermission:]` or `+[AVAudioSession sharedInstance]` to acquire
- * record permissions. If the user has denied permission or the permissions are
- * undetermined, the return value will be false and an appropriate error is
- * populated in the error pointer. The internal buffer of `AudioRecord` of
- * length bufferSize will always have the most recent audio samples acquired
- * from the microphhone if this function returns successfully.  Use:
- * `-[MPPAudioRecord readAtOffset:withSize:error:]` to get the data from the
- * buffer at any instance, if audio recording has started successfully.
+ * Before calling this function, you must use
+ * `AVAudioSession.sharedInstance.requestRecordPermission()` to acquire record permissions. If the
+ * user has denied permission or the permissions are undetermined, this method will throw an error.
+ * The internal buffer of `AudioRecord` of will always have the most recent audio samples acquired
+ * from the microphhone if this function returns successfully.  Use: `read(offset:length:)` to get
+ * the data from the buffer at any point of time after audio recording has started successfully.
  *
- * Use `-[MPPAudioRecord stop]` to stop the audio recording.
- *
- * @param error An optional error parameter populated when the microphone input
- * could not be recorded successfully.
+ * Use `stop()` to stop recording audio sample from the microphone.
  *
  * @return Boolean value indicating if audio recording started successfully.
  */
@@ -85,21 +76,19 @@ NS_SWIFT_NAME(AudioRecord)
 - (void)stop;
 
 /**
- * Returns the `length` number of elements in the internal buffer of
- * `AudioRecord` starting at `offset`, i.e, `buffer[offset:offset+length]`.
+ * Returns the `length` number of elements in the internal buffer of `AudioRecord` starting at
+ * `offset`, i.e, `buffer[offset:offset+length]`.
  *
  * @param offset Index in the buffer from which elements are to be read.
  * @param length Number of elements to be returned.
- * @param error An optional error parameter populated if the internal buffer could not be read
- * successfully.
  *
  * @return A `FloatBuffer` containing the elements of the internal buffer of `AudioRecord` in
  * the range, `buffer[offset:offset+size]`. Returns `nil` if there is an error in reading the
  * internal buffer.
  */
 - (nullable MPPFloatBuffer *)readAtOffset:(NSUInteger)offset
-                                 withLEngth:(NSUInteger)length
-                                    error:(NSError **)error;
+                               withLength:(NSUInteger)length
+                                    error:(NSError **)error NS_SWIFT_NAME(read(offset:length:));
 
 @end
 
--- mediapipe/tasks/ios/audio/core/sources/MPPAudioRecord.m
+++ mediapipe/tasks/ios/audio/core/sources/MPPAudioRecord.m
@@ -25,19 +25,34 @@ static const NSUInteger kMaximumChannelCount = 2;
   AVAudioEngine *_audioEngine;
 
   /*
-   * Specifying a custom buffer size on `AVAUdioEngine` while tapping does not take effect. Hence we
-   * are storing the returned samples in a ring buffer to acheive the desired buffer size. If the
-   * specified buffer size is shorter than the buffer size supported by `AVAudioEngine` only the
-   * most recent data of the buffer of size `bufferLength` will be stored by the ring buffer.
+   * Specifying a custom buffer size on `AVAUdioEngine` while tapping audio does not take effect.
+   * Hence we store the returned samples in a ring buffer to acheive the desired buffer length. If
+   * the specified buffer length is shorter than the buffer length supported by `AVAudioEngine` only
+   * the most recent data of the buffer of length `bufferLength` will be stored by the ring buffer.
    */
   MPPFloatRingBuffer *_floatRingBuffer;
-  dispatch_queue_t _conversionQueue;
+  /**
+   * Stores any error during buffer conversion or initial startup of the audio engine. Updated to
+   * `nil` whenever a valid buffer entry is made to the internal ring buffer. This will be returned
+   * when user tries to access an invalid buffer using `[MPPAudioRecord readAtOffset:length:error]`.
+   * Writes and reads are restricted to the same queue used to access the internal ring buffer.
+   */
   NSError *_globalError;
+
+  /**
+   * Concurrent queue that permits multiple threads to read the ring buffer and the global error
+   * simultaneously but only allows a single thread to write to these instance variables. During
+   * write, all read operations are blocked.
+   *
+   * TODO: Test if moving conversion to a dedicated queue offers any advantages in terms of
+   * performance.
+   */
+  dispatch_queue_t _convertLoadAndReadBufferQueue;
 }
 
 - (nullable instancetype)initWithaudioDataFormat:(MPPAudioDataFormat *)audioDataFormat
-                                bufferLength:(NSUInteger)bufferLength
-                                       error:(NSError **)error {
+                                    bufferLength:(NSUInteger)bufferLength
+                                           error:(NSError **)error {
   self = [super init];
   if (self) {
     if (audioDataFormat.channelCount > kMaximumChannelCount || audioDataFormat.channelCount == 0) {
@@ -68,10 +83,167 @@ static const NSUInteger kMaximumChannelCount = 2;
 
     _floatRingBuffer = [[MPPFloatRingBuffer alloc] initWithLength:_bufferLength];
 
-    // Serial Queue
-    _conversionQueue = dispatch_queue_create("org.tensorflow.lite.AudioConversionQueue", NULL);
+    // Concurrent queue which permits multiple threads to read the  audio record simultaneously.
+    // Writes are restricted to only one thread during which no reads are permitted.
+    _convertLoadAndReadBufferQueue =
+        dispatch_queue_create("org.mediapipe.AudioRecordReadWriteQueue", DISPATCH_QUEUE_CONCURRENT);
   }
   return self;
+}
+
+- (BOOL)startRecordingWithError:(NSError **)error {
+  // TODO: This API is deprecated from iOS 17.0. Update to new APIs and restrict the following
+  // code's use to versions below iOS 17.0.
+  switch ([AVAudioSession sharedInstance].recordPermission) {
+    case AVAudioSessionRecordPermissionDenied: {
+      [MPPCommonUtils createCustomError:error
+                               withCode:MPPTasksErrorCodeAudioRecordPermissionDeniedError
+                            description:@"Record permission was denied by the user. "];
+      break;
+    }
+    case AVAudioSessionRecordPermissionUndetermined: {
+      [MPPCommonUtils
+          createCustomError:error
+                   withCode:MPPTasksErrorCodeAudioRecordPermissionUndeterminedError
+                description:@"Record permissions are undertermined. Yo must use AVAudioSession's "
+                            @"requestRecordPermission() to request audio record permission from "
+                            @"the user. Please read Apple's documentation for further details"
+                            @"If record permissions are granted, you can call this "
+                            @"method in the completion handler of requestRecordPermission()."];
+      break;
+    }
+
+    case AVAudioSessionRecordPermissionGranted: {
+      [self startTappingMicrophoneWithError:error];
+      return YES;
+    }
+  }
+
+  return NO;
+}
+
+- (void)stop {
+  [[_audioEngine inputNode] removeTapOnBus:0];
+  [_audioEngine stop];
+
+  // Using strong `self` (instance variable is available through strong self)
+  // is okay since the block is shortlived and it'll release its strong reference
+  // to `self` when it finishes execution.
+  //
+  // `dispatch_barrier_async` ensures no other thread can access `_floatRingBuffer` for read and
+  // write while buffer is being cleared.
+  dispatch_barrier_async(_convertLoadAndReadBufferQueue, ^{
+    [_floatRingBuffer clear];
+  });
+}
+
+- (nullable MPPFloatBuffer *)readAtOffset:(NSUInteger)offset
+                               withLength:(NSUInteger)length
+                                    error:(NSError **)error {
+  __block MPPFloatBuffer *bufferToReturn = nil;
+  __block NSError *readError = nil;
+
+  // Using strong `self` (instance variable is available through strong self)  is okay since block
+  // is shortlived and it'll release its strong reference to `self` when it finishes execution. This
+  // allows multiple threads to read the buffer at the same time. `sync` ensures the method
+  // execution (return) is blocked until the read is completed.
+  dispatch_sync(_convertLoadAndReadBufferQueue, ^{
+    if (!_globalError) {
+      bufferToReturn = [_floatRingBuffer floatBufferWithOffset:offset length:length error:error];
+    } else {
+      *error = [_globalError copy];
+    }
+  });
+
+  return bufferToReturn;
+}
+
+- (void)startTappingMicrophoneWithError:(NSError **)error {
+  AVAudioNode *inputNode = [_audioEngine inputNode];
+  AVAudioFormat *format = [inputNode outputFormatForBus:0];
+
+  AVAudioFormat *recordingFormat = [[AVAudioFormat alloc]
+      initWithCommonFormat:AVAudioPCMFormatFloat32
+                sampleRate:self.audioDataFormat.sampleRate
+                  channels:(AVAudioChannelCount)self.audioDataFormat.channelCount
+               interleaved:YES];
+
+  AVAudioConverter *audioConverter = [[AVAudioConverter alloc] initFromFormat:format
+                                                                     toFormat:recordingFormat];
+
+  // Making self weak for the `installTapOnBus` callback.
+  __weak MPPAudioRecord *weakSelf = self;
+
+  // Setting buffer size takes no effect on the input node. This class uses a ring buffer internally
+  // to ensure the requested buffer size.
+  [inputNode installTapOnBus:0
+                  bufferSize:(AVAudioFrameCount)self.bufferLength
+                      format:format
+                       block:^(AVAudioPCMBuffer *buffer, AVAudioTime *when) {
+                         //  Getting a strong reference to `weakSelf` to conditionally execute
+                         //  conversion and ring buffer loading. If self is deallocated before the
+                         //  block is called, then `strongSelf` will be `nil`. Thereafter it is kept
+                         //  in memory until the block finishes execution.
+                         __strong MPPAudioRecord *strongSelf = weakSelf;
+                         // Check for non NULL here since argument of `dispatch_sync` cannot be
+                         // NULL. Conversion and writing to buffer is done with
+                         // `dispatch_barrier_async` to ensure that no other thread has access to
+                         // the ring buffer and global error until the write is completed.
+                         if (strongSelf) {
+                           dispatch_barrier_async(strongSelf->_convertLoadAndReadBufferQueue, ^{
+                             NSError *convertAndLoadError = nil;
+                             [strongSelf convertAndLoadBuffer:buffer
+                                          usingAudioConverter:audioConverter
+                                                        error:&convertAndLoadError];
+                             strongSelf->_globalError = convertAndLoadError;
+                           });
+                         }
+                       }];
+
+  [_audioEngine prepare];
+  [_audioEngine startAndReturnError:error];
+}
+
+- (BOOL)loadAudioPCMBuffer:(AVAudioPCMBuffer *)pcmBuffer error:(NSError **)error {
+  if (pcmBuffer.frameLength == 0) {
+    [MPPCommonUtils createCustomError:error
+                             withCode:MPPTasksErrorCodeInvalidArgumentError
+                          description:@"You may have to try with a different "
+                                      @"channel count or sample rate"];
+    return NO;
+  }
+
+  if (pcmBuffer.format.commonFormat != AVAudioPCMFormatFloat32) {
+    [MPPCommonUtils createCustomError:error
+                             withCode:MPPTasksErrorCodeInvalidArgumentError
+                          description:@"Invalid pcm buffer format."];
+    return NO;
+  }
+
+  // `pcmBuffer` is already converted to an interleaved format since this method is called after
+  // -[self bufferFromInputBuffer:usingAudioConverter:error:].
+  // If an `AVAudioPCMBuffer` is interleaved, both floatChannelData[0] and floatChannelData[1]
+  // point to the same 1d array with both channels in an interleaved format according to:
+  // https://developer.apple.com/documentation/avfaudio/avaudiopcmbuffer/1386212-floatchanneldata
+  // Hence we can safely access floatChannelData[0] to get the 1D data in interleaved fashion.
+  return [_floatRingBuffer
+      loadFloatBuffer:[[MPPFloatBuffer alloc] initWithData:pcmBuffer.floatChannelData[0]
+                                                    length:pcmBuffer.frameLength]
+               offset:0
+               length:pcmBuffer.frameLength
+                error:error];
+}
+
+- (void)convertAndLoadBuffer:(AVAudioPCMBuffer *)buffer
+         usingAudioConverter:(AVAudioConverter *)audioConverter
+                       error:(NSError **)error {
+  NSError *conversionError = nil;
+  AVAudioPCMBuffer *convertedPCMBuffer = [MPPAudioRecord bufferFromInputBuffer:buffer
+                                                           usingAudioConverter:audioConverter
+                                                                         error:error];
+  if (convertedPCMBuffer) {
+    [self loadAudioPCMBuffer:convertedPCMBuffer error:error];
+  }
 }
 
 + (AVAudioPCMBuffer *)bufferFromInputBuffer:(AVAudioPCMBuffer *)pcmBuffer
@@ -105,7 +277,7 @@ static const NSUInteger kMaximumChannelCount = 2;
                                        : @"Some error occured while processing incoming audio "
                                          @"frames.";
       [MPPCommonUtils createCustomError:error
-                                   withCode:MPPTasksErrorCodeInternalError
+                               withCode:MPPTasksErrorCodeInternalError
                             description:errorDescription];
       break;
     }
@@ -123,153 +295,6 @@ static const NSUInteger kMaximumChannelCount = 2;
     }
   }
   return nil;
-}
-
-- (BOOL)loadAudioPCMBuffer:(AVAudioPCMBuffer *)pcmBuffer error:(NSError **)error {
-  if (pcmBuffer.frameLength == 0) {
-    [MPPCommonUtils createCustomError:error
-                                 withCode:MPPTasksErrorCodeInvalidArgumentError
-                          description:@"You may have to try with a different "
-                                      @"channel count or sample rate"];
-    return NO;
-  } 
-  
-  if (pcmBuffer.format.commonFormat != AVAudioPCMFormatFloat32) {
-    [MPPCommonUtils createCustomError:error
-                                 withCode:MPPTasksErrorCodeInvalidArgumentError
-                                 description:@"Invalid pcm buffer format."];
-    return NO;
-  } 
-
-
-    // `pcmBuffer` is already converted to an interleaved format since this method is called after
-    // -[self bufferFromInputBuffer:usingAudioConverter:error:].
-    // If an `AVAudioPCMBuffer` is interleaved, both floatChannelData[0] and floatChannelData[1]
-    // point to the same 1d array with both channels in an interleaved format according to:
-    // https://developer.apple.com/documentation/avfaudio/avaudiopcmbuffer/1386212-floatchanneldata
-    // Hence we can safely access floatChannelData[0] to get the 1D data in interleaved fashion.
-  return [_floatRingBuffer loadFloatData:pcmBuffer.floatChannelData[0]
-                                   dataSize:pcmBuffer.frameLength
-                                     offset:0
-                                       size:pcmBuffer.frameLength
-                                      error:error];
-}
-
-- (void)convertAndLoadBuffer:(AVAudioPCMBuffer *)buffer
-         usingAudioConverter:(AVAudioConverter *)audioConverter
-                       error:(NSError **)error {
-  NSError *conversionError = nil;
-  AVAudioPCMBuffer *convertedPCMBuffer = [MPPAudioRecord bufferFromInputBuffer:buffer
-                                                           usingAudioConverter:audioConverter
-                                                                         error:error];
-  if (convertedPCMBuffer) {
-    [self loadAudioPCMBuffer:convertedPCMBuffer error:error];
-  }
-}
-
-- (void)startTappingMicrophoneWithError:(NSError **)error {
-  AVAudioNode *inputNode = [_audioEngine inputNode];
-  AVaudioDataFormat *format = [inputNode outputFormatForBus:0];
-
-  AVAudioFormat *recordingFormat =
-      [[AVAudioFormat alloc] initWithCommonFormat:AVAudioPCMFormatFloat32
-                                       sampleRate:self.audioDataFormat.sampleRate
-                                         channels:(AVAudioChannelCount)self.audioDataFormat.channelCount
-                                      interleaved:YES];
-
-  AVAudioConverter *audioConverter = [[AVAudioConverter alloc] initFromFormat:format
-                                                                     toFormat:recordingFormat];
-
-  // Making self weak for the `installTapOnBus` callback.
-  __weak MPPAudioRecord *weakSelf = self;
-
-  // Setting buffer size takes no effect on the input node. This class uses a ring buffer internally
-  // to ensure the requested buffer size.
-  [inputNode installTapOnBus:0
-                  bufferSize:(AVAudioFrameCount)self.bufferLength
-                      format:format
-                       block:^(AVAudioPCMBuffer *buffer, AVAudioTime *when) {
-                         // Getting a strong reference to `weakSelf` to conditionally execute
-                         // conversion and ring buffer loading. If self is deallocated before the
-                         // block is called, then `strongSelf` will be `nil`. Thereafter it is kept
-                         // in memory until the block finishes execution.
-                         __strong MPPAudioRecord *strongSelf = weakSelf;
-                         // Check here since argument of `dispatch_sync` cannot
-                         // be NULL.
-                         if (!strongSelf) {
-                           return
-                         }
-                         dispatch_sync(strongSelf.conversionQueue, ^{
-                           NSError *convertAndLoadError = nil;
-                           [strongSelf convertAndLoadBuffer:buffer
-                                        usingAudioConverter:audioConverter
-                                                      error:&convertAndLoadError];
-                           strongSelf.globalError = convertAndLoadError;
-                         });
-                       }];
-
-  [_audioEngine prepare];
-  [_audioEngine startAndReturnError:error];
-}
-
-- (BOOL)startRecordingWithError:(NSError **)error {
-  switch ([AVAudioSession sharedInstance].recordPermission) {
-    case AVAudioSessionRecordPermissionDenied: {
-      [MPPCommonUtils createCustomError:error
-                               withCode:MPPTasksErrorCodeAudioRecordPermissionDeniedError
-                            description:description:@"Record permission was denied by the user. "];
-      break;
-    }
-    case AVAudioSessionRecordPermissionUndetermined: {
-      [MPPCommonUtils
-          createCustomError:error
-                   withCode:MPPTasksErrorCodeAudioRecordPermissionUndeterminedError
-                description:description
-                           :@"Record permissions are undertermined. Yo must use AVAudioSession's "
-                            @"requestRecordPermission() to request audio record permission from "
-                            @"the user. Please read Apple's documentation for further details"
-                            @"If record permissions are granted, you can call this "
-                            @"method in the completion handler of requestRecordPermission()."];
-      break;
-    }
-
-    case AVAudioSessionRecordPermissionGranted: {
-      [self startTappingMicrophoneWithError:error];
-      return YES;
-    }
-  }
-
-  return NO;
-}
-
-- (void)stop {
-  [[self.audioEngine inputNode] removeTapOnBus:0];
-  [self.audioEngine stop];
-
-  // Using strong `self` is okay since block is shortlived and it'll release its
-  // strong reference to `self` when it finishes execution.
-  dispatch_sync(self.conversionQueue, ^{
-    [_floatRingBuffer clear];
-  });
-}
-
-- (nullable MPPFloatBuffer *)readAtOffset:(NSUInteger)offset
-                               withLength:(NSUInteger)length
-                                    error:(NSError **)error {
-  __block MPPFloatBuffer *bufferToReturn = nil;
-  __block NSError *readError = nil;
-
-  // Using strong `self` is okay since block is shortlived and it'll release its
-  // strong reference to `self` when it finishes execution.
-  dispatch_sync(self.conversionQueue, ^{
-    if (self.globalError) {
-      *error = [self.globalError copy];
-      return
-    }
-    bufferToReturn = [_floatRingBuffer readAtOffset:offset withLength:length error:error]
-  });
-
-  return bufferToReturn;
 }
 
 @end
